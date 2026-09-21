1e0aa86e6e842b9182e99808e2afa78d1de0cc6f
Wt <weverton10trademark@gmail.com>
2026-09-21 09:41:18 -0300

    Add files via upload

---
+++ LogicaProgramacao/ex3.12.py
@@ -0,0 +1,4 @@
+distancia = float(input("Digite a distância em quilometros: "))
+velocidade = float(input("Digite a velocidade em km/h: "))
+tempo = distancia / velocidade
+print(f"O tempo da viagem é: {tempo:.2f} horas")
